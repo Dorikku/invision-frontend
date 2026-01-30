c4c2b3f2c4104f7b9b7fe56e5df5cb180da3db9d
Dorikku <nifashendrickson@gmail.com>
2026-01-31 07:44:23 +0800

    change for user training

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // base: "/invision-frontend/",
-  base: "/",
+  base: "/invision-frontend/",
+  // base: "/",
 })
